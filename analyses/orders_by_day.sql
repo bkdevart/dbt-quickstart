ba13217525cc86736d479af0b7bd9e9e1baab0c1
Brandon Knox <bkdevart@gmail.com>
2024-01-29 15:20:18 +0000

    removed previous_day_orders column and generated new columns illustrating the total of shipments based on status per day

--- analyses/orders_by_day.sql
+++ analyses/orders_by_day.sql
@@ -9,22 +9,18 @@ daily as (
     select 
 
         order_date,
-        count(*) as order_num
+        count(*) as num_orders,
+
+    {% for order_status in ['returned', 'completed', 'return_pending', 'shipped', 'placed'] %} 
+
+        sum(case when status = '{{ order_status }}' then 1 else 0 end) as {{ order_status }}_total {{ ',' if not loop.last }}
+
+    {% endfor %}
 
     from orders
 
     group by 1
 
-),
-
-compared as (
-
-    select *,
-
-        lag(order_num) over (order by order_date) as previous_day_orders
-
-    from daily
-
 )
 
-select * from compared
+select * from daily
